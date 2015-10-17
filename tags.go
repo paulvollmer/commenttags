7c7c10ed8bf3ac456c04cccee49a419c982b6d53
paulvollmer <paul.vollmer@fh-potsdam.de>
2015-10-17 11:11:01 +0200

    create tags constants

---
+++ tags.go
@@ -0,0 +1,11 @@
+package commenttags
+
+// Check out the wikipedia documentation at
+// https://en.wikipedia.org/wiki/Comment_(computer_programming)#Tags
+const (
+	TagFIXME  = "FIXME"  // should be corrected
+	TagTODO   = "TODO"   // something to be done
+	TagHACK   = "HACK"   // a workaround
+	TagUNDONE = "UNDONE" // a reversal or "roll back" of previous code
+	TagXXX    = "XXX"    // should be corrected
+)
