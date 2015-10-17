ddf152c466e8958d5b2ea1bb139fc037781df7fe
paulvollmer <paul.vollmer@fh-potsdam.de>
2015-10-17 11:14:50 +0200

    Create fixture data

---
+++ fixture/sample.js
@@ -0,0 +1,18 @@
+// javascript test file
+
+// TODO: test-1
+
+/* TODO: test-2 */
+
+/**
+ * TODO: test-3
+ */
+
+console.log("hello world");
+
+var pi = 3.14 // TODO: test-4
+
+// FIXME: test-fixme
+// HACK: test-hack
+// UNDONE: test-undone
+// XXX: test-xxx
