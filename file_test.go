ff371e002e7cc174829076016a2f0d1e6f684bdd
paulvollmer <paul.vollmer@fh-potsdam.de>
2015-10-21 13:32:19 +0200

    Added Test ProcessFile_NotExist

--- file_test.go
+++ file_test.go
@@ -22,7 +22,11 @@ func Test_ProcessFile(t *testing.T) {
 	if tags.Tags[0].Message != "test-1" {
 		t.Error("ProcessFile Message not equal.")
 	}
-	// if tags.Tags[0].Line != 2 {
-	// 	t.Error("ProcessFile Line not equal.")
-	// }
+}
+
+func Test_ProcessFile_NotExist(t *testing.T) {
+	_, err := ProcessFile("./fixture/not/exist.ext")
+	if err == nil {
+		t.Error(err)
+	}
 }
