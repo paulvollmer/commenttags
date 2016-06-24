4f0b73555a011002c6cc9a1ac9b45124897ff983
Paul Vollmer <paul.vollmer@fh-potsdam.de>
2016-06-24 12:20:20 +0200

    cli now process multiple files

--- cmd/commenttags/main.go
+++ cmd/commenttags/main.go
@@ -11,94 +11,113 @@ import (
 )
 
 const (
-	Version          = "0.1.2"
+	Version          = "0.1.3"
 	SourceTypeFile   = 0
 	SourceTypeFolder = 1
 )
 
 var (
-	formatted  string
-	sourceType int
+	sourcePaths     []string
+	sourceType      int
+	flagVersion     = flag.Bool("v", false, "print out the `version`")
+	flagFormat      = flag.String("f", "pretty", "set the output `format`. (pretty, json or json-pretty)")
+	flagWrite       = flag.String("w", "", "`write` to file")
+	flagMaxFilesize = flag.Int64("m", 5000000, "the `maximum filesize` to process")
 )
 
 func main() {
-	flagVersion := flag.Bool("v", false, "print out the `version`")
-	flagFormat := flag.String("f", "pretty", "set the output `format`. (pretty, json or json-pretty)")
-	flagWrite := flag.String("w", "", "`write` to file")
-	flagMaxFilesize := flag.Int64("m", 5000000, "the `maximum filesize` to process")
+	flag.Usage = func() {
+		fmt.Printf("Usage: commenttags <flags> [sources...]\n\n")
+		flag.PrintDefaults()
+	}
 	flag.Parse()
 	if *flagVersion {
 		fmt.Println(Version)
 		return
 	}
+	sourcePaths = make([]string, 0)
+	totalArgs := len(os.Args)
 
-	if len(os.Args) > 1 {
-		//
-		// process data
-		//
-		sourcePath := os.Args[len(os.Args)-1]
-		folderResult := &commenttags.DirectoryData{}
-		fileResult, err := commenttags.ProcessFile(sourcePath)
-		if err != nil {
-			// check if directory
-			if err.Error() == "read "+sourcePath+": is a directory" {
-				sourceType = SourceTypeFolder
-				// Try to read as directory...
-				folderResult, err = commenttags.ProcessDirectory(sourcePath, *flagMaxFilesize)
-				if err != nil {
-					fmt.Println("read directory failed!", err)
-					os.Exit(1)
-				}
+	// check args and set source paths array
+	if totalArgs == 1 {
+		sourcePaths = append(sourcePaths, ".")
+	}
+	if totalArgs > 1 {
+		sourcePaths = append(sourcePaths, os.Args[1:]...)
+	}
+
+	// process data
+	// fmt.Println("Total sources to process:", len(sourcePaths), sourcePaths)
+	for _, v := range sourcePaths {
+		processSourcePath(v)
+	}
+}
+
+func processSourcePath(src string) {
+	folderResult := &commenttags.DirectoryData{}
+	fileResult, err := commenttags.ProcessFile(src)
+	if err != nil {
+		// check if directory
+		if err.Error() == "read "+src+": is a directory" {
+			sourceType = SourceTypeFolder
+			// Try to read as directory...
+			folderResult, err = commenttags.ProcessDirectory(src, *flagMaxFilesize)
+			if err != nil {
+				fmt.Println("read directory failed!", err)
+				os.Exit(1)
 			}
 		}
+	}
+	// else {
+	// 	fileResult.PrettyPrint()
+	// }
 
-		//
-		// format the result result
-		//
-		switch *flagFormat {
-		case "pretty":
-			if sourceType == SourceTypeFile {
+	//
+	// format the result result
+	//
+	var formatted string
+	switch *flagFormat {
+	case "pretty":
+		if sourceType == SourceTypeFile {
+			if fileResult.TagsFound() {
 				formatted = fileResult.Pretty()
-			} else if sourceType == SourceTypeFolder {
-				formatted += folderResult.Pretty()
 			}
-			break
-		case "json":
-			if sourceType == SourceTypeFile {
-				out, _ := fileResult.JSON()
-				formatted = string(out)
-			} else if sourceType == SourceTypeFolder {
-				tmp, _ := folderResult.JSON()
-				formatted = string(tmp)
-			}
-			break
-		case "json-pretty":
-			if sourceType == SourceTypeFile {
-				out, _ := json.MarshalIndent(fileResult, "", "  ")
-				formatted = string(out)
-			} else if sourceType == SourceTypeFolder {
-				out, _ := json.MarshalIndent(folderResult, "", "  ")
-				formatted = string(out)
-			}
-			break
-		default:
-			fmt.Printf("Format '%s' not supported! Choose between the following formats:\n", *flagFormat)
-			fmt.Println("- pretty (default)")
-			fmt.Println("- json")
-			fmt.Println("- json-pretty")
-			break
+		} else if sourceType == SourceTypeFolder {
+			formatted += folderResult.Pretty()
 		}
-
-		//
-		// write or print out the result
-		//
-		if *flagWrite != "" {
-			ioutil.WriteFile(*flagWrite, []byte(formatted), 0777)
-		} else {
-			fmt.Println(formatted)
+		break
+	case "json":
+		if sourceType == SourceTypeFile {
+			out, _ := fileResult.JSON()
+			formatted = string(out)
+		} else if sourceType == SourceTypeFolder {
+			tmp, _ := folderResult.JSON()
+			formatted = string(tmp)
 		}
+		break
+	case "json-pretty":
+		if sourceType == SourceTypeFile {
+			out, _ := json.MarshalIndent(fileResult, "", "  ")
+			formatted = string(out)
+		} else if sourceType == SourceTypeFolder {
+			out, _ := json.MarshalIndent(folderResult, "", "  ")
+			formatted = string(out)
+		}
+		break
+	default:
+		fmt.Printf("Format '%s' not supported! Choose between the following formats:\n", *flagFormat)
+		fmt.Println("- pretty (default)")
+		fmt.Println("- json")
+		fmt.Println("- json-pretty")
+		break
+	}
 
+	//
+	// write or print out the result
+	//
+	if *flagWrite != "" {
+		ioutil.WriteFile(*flagWrite, []byte(formatted), 0777)
 	} else {
-		fmt.Println("Missing Filepath, See the -h help out...")
+		fmt.Print(formatted)
 	}
 }
